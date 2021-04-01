b6569009cfc25fc0380b184db0079801284b4399
Angelos Mantzaflaris <angelos.mantzaflaris@inria.fr>
2021-04-01 16:04:49 +0200

    material class

--- gsLinearMaterial.h
+++ gsLinearMaterial.h
@@ -9,15 +9,16 @@
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
     Author(s):
-        O. Weeger    (2012 - 2015, TU Kaiserslautern),
-        A.Shamanskiy (2016 - 2020, TU Kaiserslautern),
-        H.M.Verhelst (2019 - ...., TU Delft)
+    O. Weeger    (2012 - 2015, TU Kaiserslautern),
+    A.Shamanskiy (2016 - 2020, TU Kaiserslautern),
+    H.M.Verhelst (2019 - ...., TU Delft)
 */
 
 #pragma once
 
 #include <gsElasticity/gsMaterialBase.h>
 #include <gsElasticity/gsVisitorElUtils.h>
+#include <gsUtils/gsThreaded.h>
 
 namespace gismo
 {
@@ -31,40 +32,51 @@ public:
 
     gsLinearMaterial(   const T E,
                         const T nu)
+    : m_homogeneous(true)
     {
-        m_Emodulus = E;
-        m_PoissonRatio = nu;
+        m_Emodulus.push_back(E);
+        m_PoissonRatio.push_back(nu);
     }
 
+    gsLinearMaterial(std::vector<T> E,
+                     std::vector<T> nu)
+    : m_Emodulus(give(E)), m_PoissonRatio(give(nu)), m_homogeneous(true)
+    {
+    }
+
+    mutable util::gsThreaded<gsMatrix<T> > C, Ctemp;
+    
     /// \a u points, \a k patch index
     void eval_matrix_into(const gsMatrix<T>& u, gsMatrix<T>& result, index_t k = 0) const
     {
+        if (m_homogeneous) k = 0;
+        GISMO_ASSERT(m_Emodulus.size()<static_cast<size_t>(k), "Invalid patch index");
+
         const index_t sz = (u.rows()+1)*u.rows()/2;
         result.resize(sz*sz,u.cols());
 
-        gsMatrix<T> C, Ctemp;
-        T lambda = m_Emodulus * m_PoissonRatio / ( ( 1. + m_PoissonRatio ) * ( 1. - 2. * m_PoissonRatio ) );
-        T mu     = m_Emodulus / ( 2. * ( 1. + m_PoissonRatio ) );
+        T lambda = m_Emodulus.at(k) * m_PoissonRatio.at(k) / ( ( 1. + m_PoissonRatio.at(k) ) * ( 1. - 2. * m_PoissonRatio.at(k) ) );
+        T mu     = m_Emodulus.at(k) / ( 2. * ( 1. + m_PoissonRatio.at(k) ) );
         // linear elasticity tensor
         gsMatrix<T> I = gsMatrix<T>::Identity(u.rows(),u.rows());
         matrixTraceTensor<T>(C,I,I);
-        C *= lambda;
+        C.mine() *= lambda;
         symmetricIdentityTensor<T>(Ctemp,I);
-        C += mu*Ctemp;
+        C.mine() += mu*Ctemp.mine();
 
-        for (index_t k=0; k!=u.cols(); k++)
-            result.reshapeCol(k,sz,sz) = C;
-
+        for (index_t j=0; j!=u.cols(); j++)
+            result.reshapeCol(j,sz,sz) = C.mine();
     }
 
-    /// \a u points, \a k patch index
-    void eval_vector_into(const gsMatrix<T>& u, gsMatrix<T>& result, index_t k = 0) const
+        /// \a u points, \a k patch index
+        void eval_vector_into(const gsMatrix<T>& u, gsMatrix<T>& result, index_t k = 0) const
     {
         GISMO_NO_IMPLEMENTATION;
     }
 
 protected:
-    T m_Emodulus, m_PoissonRatio;
+    bool m_homogeneous;
+    std::vector<T> m_Emodulus, m_PoissonRatio;
 
 };
 
