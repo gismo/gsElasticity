8484e4a383b2d39ce73968fbd8a4805c39c92359
Hugo Verhelst <hugoverhelst1995@gmail.com>
2021-04-23 13:45:39 +0200

    gsCompositeMatrix Raw

--- gsCompositeMatrix.cpp
+++ gsCompositeMatrix.cpp
@@ -52,6 +52,48 @@ gsMatrix<T> gsCompositeMatrix(	const T Exx,
 }
 
 template<class T>
+gsMatrix<T> gsCompositeMatrixRaw(	const T G11,const T G12,const T G13,const T G14,const T G15,const T G16,
+												const T G22,const T G23,const T G24,const T G25,const T G26,
+	                            							const T G33,const T G34,const T G35,const T G36,
+	                            										const T G44,const T G45,const T G46,
+	                            													const T G55,const T G56,
+	                            																const T G66
+	                          	)
+{
+	gsMatrix<T> G(6,6);
+	G.setZero();
+	G(0,1) = G12;
+	G(0,2) = G13;
+	G(0,3) = G14;
+	G(0,4) = G15;
+	G(0,5) = G16;
+
+	G(1,2) = G23;
+	G(1,3) = G24;
+	G(1,4) = G25;
+	G(1,5) = G26;
+
+	G(2,3) = G34;
+	G(2,4) = G35;
+	G(2,5) = G36;
+
+	G(3,4) = G45;
+	G(3,5) = G46;
+
+	G(4,5) = G56;
+
+	G = G + G.transpose();
+	G(0,0) = G11;
+	G(1,1) = G22;
+	G(2,2) = G33;
+	G(3,3) = G44;
+	G(4,4) = G55;
+	G(5,5) = G66;
+
+	return G;
+}
+
+template<class T>
 gsMatrix<T> gsCompositeMatrix(	const T Exx,
 	                            const T Eyy,
 	                            const T Gxy,
@@ -66,5 +108,26 @@ gsMatrix<T> gsCompositeMatrix(	const T Exx,
 	G(0,1) = G(1,0) = (nuxy*Exx) / ((1+nuxy)*(1-2*nuxy)); // Gxy
 
 	G(2,2) = Gxy;
+	return G;
+}
+
+template<class T>
+gsMatrix<T> gsCompositeMatrixRaw(	const T G11,const T G12,const T G13,
+												const T G22,const T G23,
+	                            							const T G33
+	                          	)
+{
+	gsMatrix<T> G(3,3);
+	G.setZero();
+	G(0,1) = G12;
+	G(0,2) = G13;
+
+	G(1,2) = G23;
+
+	G = G + G.transpose();
+	G(0,0) = G11;
+	G(1,1) = G22;
+	G(2,2) = G33;
+
 	return G;
 }
