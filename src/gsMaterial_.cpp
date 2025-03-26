18889967b17c557e46911a1df938854cdf39122b
Hugo Verhelst <38376601+hverhelst@users.noreply.github.com>
2025-03-26 14:07:38 +0100

    Update gsMaterial_.cpp

--- src/gsMaterial_.cpp
+++ src/gsMaterial_.cpp
@@ -1,14 +1,12 @@
 #include <gsCore/gsTemplateTools.h>
 
 #include <gsElasticity/gsLinearMaterial.h>
-#include <gsElasticity/gsLinearDegradedMaterial.h>
 #include <gsElasticity/gsNeoHookeLogMaterial.h>
 #include <gsElasticity/gsNeoHookeQuadMaterial.h>
 
 namespace gismo
 {
   CLASS_TEMPLATE_INST gsLinearMaterial<real_t>;
-  CLASS_TEMPLATE_INST gsLinearDegradedMaterial<real_t>;
   CLASS_TEMPLATE_INST gsNeoHookeLogMaterial<real_t>;
   CLASS_TEMPLATE_INST gsNeoHookeQuadMaterial<real_t>;
 
